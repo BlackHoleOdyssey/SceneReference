63791f4e733bb77b4a61b6a31def8d89841f3f92
Cedrico <118186038+Cedricoooo@users.noreply.github.com>
2026-04-19 20:29:55 +0200

    Clear auto generated script and fix reference

--- Scripts/Runtime/Generated/SceneRegistryGenerated.cs
+++ Scripts/Runtime/Generated/SceneRegistryGenerated.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 namespace BHO.SceneReference {
     public static class SceneRegistryGenerated {
         public static readonly Dictionary<string, int> Scenes = new() {
-            { "33799e23c259c9c4aa980cd2bbca6da4", 0 },
         };
     }
 }
